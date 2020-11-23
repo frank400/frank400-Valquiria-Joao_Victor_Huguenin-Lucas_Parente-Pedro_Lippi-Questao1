f952fecf1bc4e063fe792de387006707981f370f
frank400 <j.victorhuguenin2018@gmail.com>
2020-11-23 20:55:41 -0300

    feat: adiciona funcao de limpar tela

--- question1.c
+++ question1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h> 
 
+void clear(){
+    // funcao para limpar a tela para ambos sistemas operacionais.
+    #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+        system("clear");
+    #endif
+
+    #if defined(_WIN32) || defined(_WIN64)
+        system("cls");
+    #endif
+}
 int main()
 {
     
