102eec46f3882441c8048495ebe3d9bd005b7946
frank400 <j.victorhuguenin2018@gmail.com>
2020-11-29 21:17:24 -0300

    feat: cria uma biblioteca para operações envolvendo o terminal

---
+++ src/system_operations.c
@@ -0,0 +1,29 @@
+#include "system_operations.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void delay(int number_of_seconds) {
+#if defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+    Sleep(number_of_seconds*1000000);
+#else
+
+#include <unistd.h>
+    usleep(1000000 * number_of_seconds);
+#endif
+}
+
+void clear() {
+    // funcao para limpar a tela para ambos sistemas operacionais.
+#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+    system("clear");
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+    system("cls");
+#endif
+}
+
+
---
+++ src/system_operations.h
@@ -0,0 +1,8 @@
+#ifndef JOGO_DA_VIDA_SYSTEM_OPERATIONS_H
+#define JOGO_DA_VIDA_SYSTEM_OPERATIONS_H
+
+
+void delay(int number_of_seconds);
+void clear();
+
+#endif //JOGO_DA_VIDA_SYSTEM_OPERATIONS_H
